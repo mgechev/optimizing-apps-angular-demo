eb356dabea4eb4dbfd0cf3a817301f66859e6bae
mgechev <mgechev@gmail.com>
2024-12-06 01:07:20 +0100

    Use latest material

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatLegacyTableModule as MatTableModule } from '@angular/material/legacy-table';
+import { MatTableModule } from '@angular/material/table';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
